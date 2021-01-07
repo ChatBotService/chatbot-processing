452866868cf7f47a803736c23f0038a54d58e482
JureBevc <bevc.jure@gmail.com>
2021-01-07 03:29:01 +0100

    Update

--- api/processing_api.py
+++ api/processing_api.py
@@ -29,17 +29,19 @@ class ProcessingAPI(Resource):
         # Add process to queue
         file_id = request.args.get("id")
         name = request.args.get("name")
+        print(file_id, flush=True)
+        print(name, flush=True)
         if file_id:
             conversations = ConversationFile.query.filter_by(id=file_id).first()
             if conversations:
                 if processing.in_queue(int(file_id)):
                         return Response(f"File with id {file_id} already in queue.",status=200)  
-                processing.add_to_queue({"conversations":conversations, "name" : name})
+                processing.add_to_queue({"conversation":conversations, "name" : name})
                 return Response(f"File with id {file_id} was added to queue.",status=200)
             else:
                 return Response("Bad Request:\nCould not find or load existing conversation file.",status=400)
         else:
-            return Response("Bad Request:\nCould not find or load existing conversation file.",status=400)
+            return Response("Bad Request:\nBad file id.",status=400)
     
     def delete(self):
         # Remove a process from queue
